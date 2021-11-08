e462513093d9b4f453275d3b6584dc5e3e835df9
Patrick Abernathy <pat.abs@gmail.com>
2021-11-07 19:43:35 -0600

    Updates

---
+++ precache-manifest.fc36f0f74c57161276789ee07971f478.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35a618b145b3058c75d6c30c5727243",
+    "url": "/pa-react-portfolio/index.html"
+  },
+  {
+    "revision": "f1b4efbeea42dbb4780a",
+    "url": "/pa-react-portfolio/static/css/main.9d9bfbed.chunk.css"
+  },
+  {
+    "revision": "ee2e22ecc6254efa6f00",
+    "url": "/pa-react-portfolio/static/js/2.66f3dd55.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pa-react-portfolio/static/js/2.66f3dd55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b4efbeea42dbb4780a",
+    "url": "/pa-react-portfolio/static/js/main.5f6c4346.chunk.js"
+  },
+  {
+    "revision": "2a001b710cc69066bf2b",
+    "url": "/pa-react-portfolio/static/js/runtime-main.d2dc4dd8.js"
+  },
+  {
+    "revision": "978fe3d826589f2303b12885b1bdf1a1",
+    "url": "/pa-react-portfolio/static/media/ThankYou.978fe3d8.jpg"
+  },
+  {
+    "revision": "e85c49692f60c21f12c5c64cb36770b2",
+    "url": "/pa-react-portfolio/static/media/absius-logo.e85c4969.png"
+  },
+  {
+    "revision": "e50c3f63e028fe6b98e30b47f3df8332",
+    "url": "/pa-react-portfolio/static/media/email.e50c3f63.png"
+  },
+  {
+    "revision": "725bd73ce172253e85630c1fc5788a67",
+    "url": "/pa-react-portfolio/static/media/gameSearch.725bd73c.png"
+  },
+  {
+    "revision": "3480db5eabd3ef35cf349caa44c5171e",
+    "url": "/pa-react-portfolio/static/media/github.3480db5e.png"
+  },
+  {
+    "revision": "195ca909ef8e9c2269646a9b06d32c84",
+    "url": "/pa-react-portfolio/static/media/pat-professional.195ca909.jpg"
+  },
+  {
+    "revision": "3adfa0c73972c6e28a818a0009140582",
+    "url": "/pa-react-portfolio/static/media/run.3adfa0c7.jpg"
+  },
+  {
+    "revision": "f31b71b2363e15d216feb848a44c8a07",
+    "url": "/pa-react-portfolio/static/media/tristan-smile.f31b71b2.jpg"
+  },
+  {
+    "revision": "dc5243a1256e3356213957a74c897ea9",
+    "url": "/pa-react-portfolio/static/media/virtual-guardian.dc5243a1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pa-react-portfolio/precache-manifest.3673c9b4c2efb27cd35184e2a8f8d0b1.js"
+  "/pa-react-portfolio/precache-manifest.fc36f0f74c57161276789ee07971f478.js"
 );
 
 self.addEventListener('message', (event) => {
