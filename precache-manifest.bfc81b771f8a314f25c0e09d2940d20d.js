987d6f187313eb04f148bd65462459ee4011a042
Patrick Abernathy <pat.abs@gmail.com>
2021-11-07 19:10:29 -0600

    Updates

---
+++ precache-manifest.bfc81b771f8a314f25c0e09d2940d20d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7320d185e7200d286e736112af28cf9",
+    "url": "/pa-react-portfolio/index.html"
+  },
+  {
+    "revision": "eed5da05c3e6f71dd195",
+    "url": "/pa-react-portfolio/static/css/main.79693ddb.chunk.css"
+  },
+  {
+    "revision": "58024e53f576118ab2c7",
+    "url": "/pa-react-portfolio/static/js/2.2f71a7a7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pa-react-portfolio/static/js/2.2f71a7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed5da05c3e6f71dd195",
+    "url": "/pa-react-portfolio/static/js/main.dfad5d51.chunk.js"
+  },
+  {
+    "revision": "2a001b710cc69066bf2b",
+    "url": "/pa-react-portfolio/static/js/runtime-main.d2dc4dd8.js"
+  },
+  {
+    "revision": "e85c49692f60c21f12c5c64cb36770b2",
+    "url": "/pa-react-portfolio/static/media/absius-logo.e85c4969.png"
+  },
+  {
+    "revision": "e50c3f63e028fe6b98e30b47f3df8332",
+    "url": "/pa-react-portfolio/static/media/email.e50c3f63.png"
+  },
+  {
+    "revision": "195ca909ef8e9c2269646a9b06d32c84",
+    "url": "/pa-react-portfolio/static/media/pat-professional.195ca909.jpg"
+  }
+]);
